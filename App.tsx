700ca8a76249480036939daa8be4741034f33115
권신학 <cheatkey33@gmail.com>
2023-06-30 00:32:10 +0900

    rn 과 next 웹 통신 예시 추가

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, {useRef} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import WebView from 'react-native-webview';
 
 const App = (): JSX.Element => {
+  const webViewRef = useRef(null);
+
   return (
     <SafeAreaView style={styles.container}>
       <WebView
+        ref={webViewRef}
         style={styles.webview}
         source={{uri: 'http://localhost:3000/'}}
+        onLoadEnd={syntheticEvent => {
+          console.log('로딩 끗');
+        }}
+        onMessage={event => {
+          alert(event.nativeEvent.data);
+          webViewRef.current?.postMessage('안녕');
+        }}
       />
     </SafeAreaView>
   );
